f73c967e2533f25a332c4e965197b35e0910066b
arcefranco <arcefranco29@gmail.com>
2022-07-10 21:56:19 -0300

    config dev

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,7 +1,11 @@
 import express from 'express'
 import tasksRoutes from './routes/tasks'
-const app = express();
+import cors from 'cors'
+import morgan from 'morgan'
 
+const app = express();
+app.use(cors())
+app.use(morgan('dev'))
 app.use(express.json())
 app.use(tasksRoutes)
 
--- back-end/src/config.js
+++ back-end/src/config.js
@@ -1,8 +1,12 @@
+import {config as dotenv} from 'dotenv'
+
+dotenv()
+
 const config = {
-    host: 'localhost',
-    user: 'root',
-    password: 'Gasometro10',
-    database: 'todonative'
+    host: process.env.PORT,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
 }
 
 export default config
